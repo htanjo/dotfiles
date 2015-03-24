2a0896118b8b16c28939c406ba228228a50ca675
Tanjo, Hiroyuki <expheno@gmail.com>
2015-03-24 11:38:06 +0900

    Add ranger config

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.oh-my-zsh/custom/themes ~/.oh-my-zsh/custom/themes
 ln -s ~/dotfiles/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 ln -s ~/dotfiles/.vim/colors ~/.vim/colors
+ln -s ~/dotfiles/.config/ranger/rc.conf ~/.config/ranger/rc.conf
