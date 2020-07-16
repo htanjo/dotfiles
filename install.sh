ec9cba8bb7862cd50b1ade8381988d189f633a97
Hiroyuki Tanjo <hiroyuki.tanjo@rakuten.com>
2020-07-16 15:54:44 +0900

    Update install script.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.oh-my-zsh/custom/themes ~/.oh-my-zsh/custom/themes
-ln -s ~/dotfiles/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+ln -s ~/dotfiles/.oh-my-zsh/custom/themes/* ~/.oh-my-zsh/custom/themes/
+ln -s ~/dotfiles/.oh-my-zsh/custom/plugins/* ~/.oh-my-zsh/custom/plugins/
 ln -s ~/dotfiles/.vim/colors ~/.vim/colors
 ln -s ~/dotfiles/.config/ranger/rc.conf ~/.config/ranger/rc.conf
