3b1147729f629037aa7834828753f7ddde94ca3b
Hiroyuki Tanjo <expheno@gmail.com>
2025-01-22 15:31:38 +0900

    Update install.sh for .gitignore_global

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/dotfiles/.oh-my-zsh/custom/themes/* ~/.oh-my-zsh/custom/themes/
 ln -s ~/dotfiles/.oh-my-zsh/custom/plugins/* ~/.oh-my-zsh/custom/plugins/
 ln -s ~/dotfiles/.vim/colors ~/.vim/colors
